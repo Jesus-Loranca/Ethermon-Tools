7a886ac1a6a4aa8ac05886109320b54dfa800a34
Jesús A. Loranca <jesus.creatiboom@gmail.com>
2022-05-12 19:12:18 +0200

    update: Mons data according to the type changes

--- src/app/data/mons.ts
+++ src/app/data/mons.ts
@@ -1,48 +1,49 @@
-export const mons = {
-	dilloom : {
+import { Mon } from './types/mon';
+
+export const mons: Array<Mon> = [
+	{
 		id: 1,
-		types : [
-			'leaf',
-			'toxin'
-		],
-		ancestors : [],
+		name: 'Dilloom',
+		types: ['leaf', 'toxin'],
+		ancestors: [],
 		stats: {
 			hp: [82, 6],
 			pa: [83, 3],
 			pd: [83, 3],
 			sa: [99, 3],
 			sd: [102, 6],
-			sp: [82, 6]
+			sp: [82, 6],
 		},
+		bp: 0,
 	},
-	dynamouse : {
+	{
 		id: 2,
-		types : [
-			'fire'
-		],
-		ancestors : [],
+		name: 'Dynamouse',
+		types: ['fire'],
+		ancestors: [],
 		stats: {
 			hp: [76, 6],
 			pa: [89, 6],
 			pd: [77, 3],
 			sa: [97, 6],
 			sd: [94, 3],
-			sp: [99, 3]
+			sp: [99, 3],
 		},
+		bp: 0,
 	},
-	nageel : {
+	{
 		id: 3,
-		types : [
-			'fire'
-		],
-		ancestors : [],
+		name: 'Nageel',
+		types: ['fire'],
+		ancestors: [],
 		stats: {
 			hp: [78, 3],
 			pa: [82, 3],
 			pd: [102, 6],
 			sa: [87, 6],
 			sd: [101, 6],
-			sp: [77, 3]
+			sp: [77, 3],
 		},
+		bp: 0,
 	},
-};
+];
