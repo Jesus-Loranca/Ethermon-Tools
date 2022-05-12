11254fc866d96ecdb0e774e7e05064228276d446
Jesús A. Loranca <jesus.creatiboom@gmail.com>
2022-05-12 17:02:45 +0200

    add: Initil mons data

---
+++ src/app/data/mons.ts
@@ -0,0 +1,48 @@
+export const mons = {
+	dilloom : {
+		id: 1,
+		types : [
+			'leaf',
+			'toxin'
+		],
+		ancestors : [],
+		stats: {
+			hp: [82, 6],
+			pa: [83, 3],
+			pd: [83, 3],
+			sa: [99, 3],
+			sd: [102, 6],
+			sp: [82, 6]
+		},
+	},
+	dynamouse : {
+		id: 2,
+		types : [
+			'fire'
+		],
+		ancestors : [],
+		stats: {
+			hp: [76, 6],
+			pa: [89, 6],
+			pd: [77, 3],
+			sa: [97, 6],
+			sd: [94, 3],
+			sp: [99, 3]
+		},
+	},
+	nageel : {
+		id: 3,
+		types : [
+			'fire'
+		],
+		ancestors : [],
+		stats: {
+			hp: [78, 3],
+			pa: [82, 3],
+			pd: [102, 6],
+			sa: [87, 6],
+			sd: [101, 6],
+			sp: [77, 3]
+		},
+	},
+};
