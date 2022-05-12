2ac793dd63d663abe507684cab8850561ff6247b
Jesús A. Loranca <jesus.creatiboom@gmail.com>
2022-05-12 19:07:38 +0200

    add: Mon interface

---
+++ src/app/data/types/mon.ts
@@ -0,0 +1,15 @@
+export interface Mon {
+	id: number,
+	name: string,
+	types : Array<string>,
+	ancestors : Array<string>,
+	stats: {
+		hp: Array<number>,
+		pa: Array<number>,
+		pd: Array<number>,
+		sa: Array<number>,
+		sd: Array<number>,
+		sp: Array<number>
+	},
+	bp: number
+};
