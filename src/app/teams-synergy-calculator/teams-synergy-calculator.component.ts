609d9aba76d2d1987796f3f0bb47511d19cfa4f0
Jesús A. Loranca <jesus.creatiboom@gmail.com>
2022-05-26 17:20:08 +0200

    add: Team type

--- src/app/teams-synergy-calculator/teams-synergy-calculator.component.ts
+++ src/app/teams-synergy-calculator/teams-synergy-calculator.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Team } from 'src/data/types/team';
 
 @Component({
 	selector: 'teams-synergy-calculator',
 	templateUrl: './teams-synergy-calculator.component.html'
 })
 export class TeamsSynergyCalculatorComponent {
-
+	synergyTeams: Array<Team> = [];
 }
---
+++ src/data/types/team.ts
@@ -0,0 +1,7 @@
+import { Mon } from "./mon";
+
+export interface Team {
+	synergy: number;
+	bp: number;
+	mons: Array<Mon>;
+}
